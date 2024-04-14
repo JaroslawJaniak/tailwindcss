9f5f5e3f9be343e5ac46c1c036dd6be4b201fe3e
JaroslawJaniak <JaroslawJaniak>
2024-04-14 21:52:03 +0200

    init commit React app to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tailwindcss ReactApp
         </a>
       </header>
     </div>
